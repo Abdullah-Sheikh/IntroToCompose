0164409770b19b7bdaec15966821baa8b0b3ac0c
F180243 <f180243@nu.edu.pk>
2022-10-24 15:56:25 +0500

    On tap increment UI completed

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.introtocompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,14 +28,48 @@ class MainActivity : ComponentActivity() {
             IntroToComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    color = Color(0xFF546E74)
                 ) {
-                    Greeting("Android")
+                    Column(verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "$100",
+                        color = Color.White,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 35.sp)
+                        Spacer(modifier = Modifier.height(130.dp))
+
+                        CreateCircle()
+                    }
+
                 }
             }
         }
     }
+}
+
+
+@Preview
+@Composable
+fun CreateCircle()
+{
+        Card(modifier = Modifier
+            .size(105.dp)
+            .padding(3.dp)
+            .clickable { Log.d("Tap", "CreateCircle: Tap ") },
+            elevation = 4.dp,
+
+            shape = CircleShape
+        ) {
+            Box(contentAlignment = Alignment.Center) {
+
+                Text(text = "Tap",  modifier = Modifier)
+            }
+            
+        }
 }
 
 @Composable
@@ -35,7 +77,6 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview
 @Composable
 fun ShowAge(age: Int = 12)
 {
@@ -47,8 +88,7 @@ fun ShowAge(age: Int = 12)
 fun DefaultPreview() {
     IntroToComposeTheme {
         Column {
-            Greeting("Android")
-            ShowAge(34)
+            CreateCircle()
         }
 
     }
